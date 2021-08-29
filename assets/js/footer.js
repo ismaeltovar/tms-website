149eac3c0dd2f788362b8e8fee72253f9a4dde7c
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2021-08-29 12:31:04 -0500

    Modified styling & positioning of the footer-component

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -7,8 +7,8 @@ class Footer extends HTMLElement {
         this.innerHTML = `
             <footer>
                 <div class="footer-text">
-                    <h3 class="copyright">Copyright &copy 2021 <a href="https://ismaeltovar.com/aboutme/" target="_blank">Ismael Tovar</a></h3>
-                    <h3>Hosted using <a href="https://github.com/ismaeltovar/tms-website" target="_blank">Github Pages</a>.</h3>
+                    <h3 class="cr-notice">Copyright &copy 2021 <a href="https://ismaeltovar.com/aboutme/" target="_blank">Ismael Tovar</a></h3>
+                    <h3 class="hosting-notice">Hosted using <a href="https://github.com/ismaeltovar/tms-website" target="_blank">Github Pages</a>.</h3>
                 </div>
             </footer>
         `;
